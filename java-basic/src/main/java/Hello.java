83809f0f019a38b62dc7953604692ca3736e3d1f
rollingball211 <jingoo211@gmail.com>
2021-06-30 17:44:12 +0900

    .

---
+++ java-basic/src/main/java/Hello.java
@@ -0,0 +1,9 @@
+class Hello{
+    //main()
+    //프로그램 실행 시작점
+    //entry point
+    public static void main(String [] args){
+        System.out.println("Hello world!,안뇽");
+        System.out.println("Hello world!,하이");
+    }
+}
