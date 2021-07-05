4071c2cc07725e65200994b7cfa83416ef61eefe
rollingball211 <jingoo211@gmail.com>
2021-07-05 18:07:20 +0900

    .

---
+++ study-project/app/src/main/java/com/eomcs/pms/App2.java
@@ -0,0 +1,14 @@
+package com.eomcs.pms;
+
+public class App2 {
+
+  public static void main(String[] args) {
+    System.out.println("프로젝트 : 미니프로젝트 관리 시스템 개발");
+    System.out.println("번호 : 1 ");
+    System.out.println("내용: 요구사항 수집");
+    System.out.println("완료일:2020-001-20");
+    System.out.println("상태 : 진행중");
+    System.out.println("담당자: 홍길`동");
+  }
+
+}
