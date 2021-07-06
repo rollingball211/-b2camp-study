552d96e2d6999f7178dddf97e50a09f270ba996a
rollingball211 <jingoo211@gmail.com>
2021-07-06 17:37:50 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/Hello3.java
+++
@@ -1,7 +0,0 @@
-package com.eomcs.lang;
-
-class Hello3 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
--- java-basic/src/main/java/com/eomcs/lang/ex01/Exam0110.java
+++
@@ -1,3 +0,0 @@
-package com.eomcs.lang.ex01;
-
-class Exam0110 {}
