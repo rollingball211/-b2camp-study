03e4ed6b4c805e820b3119d2d801bd5321be2a16
rollingball211 <jingoo211@gmail.com>
2021-07-23 13:15:52 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -10,14 +10,7 @@ public class App {
 
   public static void main(String[] args) {
 
-    // 각 게시판의 게시글을 담을 메모리(boards 레퍼런스 배열과 size)를 준비한다.
     BoardHandler boardHandler = new BoardHandler();
-    BoardHandler boardHandler2 = new BoardHandler();
-    BoardHandler boardHandler3 = new BoardHandler();
-    BoardHandler boardHandler4 = new BoardHandler();
-    BoardHandler boardHandler5 = new BoardHandler();
-    BoardHandler boardHandler6 = new BoardHandler();
-    
     MemberHandler memberHandler = new MemberHandler();
     ProjectHandler projectHandler = new ProjectHandler();
     TaskHandler taskHandler = new TaskHandler();
@@ -33,12 +26,30 @@ public class App {
 
       } else if (input.equals("/member/list")) {
         memberHandler.list();
-      
+
+      } else if (input.equals("/member/detail")) {
+        memberHandler.detail();
+
+      } else if (input.equals("/member/update")) {
+        memberHandler.update();
+
+      } else if (input.equals("/member/delete")) {
+        memberHandler.delete();
+
       }  else if (input.equals("/project/add")) {
         projectHandler.add(memberHandler);
 
       }  else if (input.equals("/project/list")) {
         projectHandler.list();
+
+      }  else if (input.equals("/project/detail")) {
+        projectHandler.detail();
+
+      }  else if (input.equals("/project/update")) {
+        projectHandler.update(memberHandler);
+
+      }  else if (input.equals("/project/delete")) {
+        projectHandler.delete();
 
       }  else if (input.equals("/task/add")) {
         taskHandler.add(memberHandler);
@@ -46,56 +57,29 @@ public class App {
       }  else if (input.equals("/task/list")) {
         taskHandler.list();
 
+      }  else if (input.equals("/task/detail")) {
+        taskHandler.detail();
+
+      }  else if (input.equals("/task/update")) {
+        taskHandler.update(memberHandler);
+
+      }  else if (input.equals("/task/delete")) {
+        taskHandler.delete();
+
       }  else if (input.equals("/board/add")) {
-        // BoardHandler의 add()를 실행할 때 
-        // add()에서 사용할 게시글 배열이 있는 인스턴스 주소를 넘겨준다.
         boardHandler.add();
 
       }  else if (input.equals("/board/list")) {
-        // BoardHandler의 list()를 실행할 때 
-        // list()에서 사용할 게시글 배열이 있는 인스턴스 주소를 넘겨준다.
         boardHandler.list();
-      } else if (input.equals("/board/detail")) {
+
+      }  else if (input.equals("/board/detail")) {
         boardHandler.detail();
-      
-      }else if(input.equals("/board/update")){
+
+      }  else if (input.equals("/board/update")) {
         boardHandler.update();
-        
-      }else if(input.equals("/board/delete")){
+
+      }  else if (input.equals("/board/delete")) {
         boardHandler.delete();
-      
-      }else if (input.equals("/board2/add")) {
-      
-        // BoardHandler의 add()를 실행할 때 
-        // add()에서 사용할 게시글 배열이 있는 인스턴스 주소를 넘겨준다.
-        boardHandler2.add();
-
-      }  else if (input.equals("/board2/list")) {
-        boardHandler2.list();
-
-      }  else if (input.equals("/board3/add")) {
-        boardHandler3.add();
-
-      }  else if (input.equals("/board3/list")) {
-        boardHandler3.list();
-
-      }  else if (input.equals("/board4/add")) {
-        boardHandler4.add();
-
-      }  else if (input.equals("/board4/list")) {
-        boardHandler4.list();
-
-      }  else if (input.equals("/board5/add")) {
-        boardHandler5.add();
-
-      }  else if (input.equals("/board5/list")) {
-        boardHandler5.list();
-
-      }  else if (input.equals("/board6/add")) {
-        boardHandler6.add();
-
-      }  else if (input.equals("/board6/list")) {
-        boardHandler6.list();
 
       } else {
         System.out.println("실행할 수 없는 명령입니다.");
--- study-project/app/src/main/java/com/eomcs/pms/handler/TaskHandler.java
+++ study-project/app/src/main/java/com/eomcs/pms/handler/TaskHandler.java
@@ -1,18 +1,18 @@
 package com.eomcs.pms.handler;
 
+import java.sql.Date;
 import com.eomcs.pms.domain.Task;
 import com.eomcs.util.Prompt;
 
 public class TaskHandler {
 
   static final int MAX_LENGTH = 5;
-  
-  
+
   Task[] tasks = new Task[MAX_LENGTH];
   int size = 0;
 
   //다른 패키지에 있는 App 클래스가 다음 메서드를 호출할 수 있도록 공개한다.
-  public  void add(MemberHandler memberHandler) {
+  public void add(MemberHandler memberHandler) {
     System.out.println("[작업 등록]");
 
     Task task = new Task();
@@ -27,45 +27,200 @@ public class TaskHandler {
     System.out.println("2: 완료");
     task.status = Prompt.inputInt("> ");
 
-    while (true) {
-      String owner = Prompt.inputString("담당자?(취소: 빈 문자열) ");
-      if (owner.length() == 0) {
-        System.out.println("작업 등록을 취소합니다.");
-        return; // 현재 메서드의 실행을 멈추고 리턴한다.
-      } else if (memberHandler.exist(owner)) {
-        task.owner = owner;
-        break;
-      }
-      System.out.println("등록된 회원이 아닙니다.");
+    task.owner = promptOwner(memberHandler, null);
+    if (task.owner == null) {
+      System.out.println("프로젝트 등록을 취소합니다.");
+      return;
     }
+    
 
-    tasks[size++] = task;
+    this.tasks[this.size++] = task;
   }
 
   //다른 패키지에 있는 App 클래스가 다음 메서드를 호출할 수 있도록 공개한다.
-  public  void list() {
+  public void list() {
     System.out.println("[작업 목록]");
 
     for (int i = 0; i < this.size; i++) {
-      String stateLabel = null;
-      switch (this.tasks[i].status) {
-        case 1:
-          stateLabel = "진행중";
-          break;
-        case 2:
-          stateLabel = "완료";
-          break;
-        default:
-          stateLabel = "신규";
-      }
 
       System.out.printf("%d, %s, %s, %s, %s\n",
           this.tasks[i].no, 
           this.tasks[i].content, 
           this.tasks[i].deadline, 
-          stateLabel, 
+          getStatusLabel(this.tasks[i].status),
           this.tasks[i].owner);
     }
   }
 
-}
+
+  public void detail() {
+    System.out.println("[작업 상세보기]");
+    int no = Prompt.inputInt("번호? ");
+
+    Task task = findByNo(no);
+    
+
+    if (task == null) {
+      System.out.println("해당 번호의 작업이 없습니다.");
+      return;
+    }
+
+    System.out.printf("내용: %s\n", task.content);
+    System.out.printf("마감일: %s\n", task.deadline);
+
+    
+    System.out.printf("상태: %s\n", getStatusLabel(task.status));
+
+    System.out.printf("담당자: %s\n", task.owner);
+  }
+
+  public void update(MemberHandler memberHandler) {
+    System.out.println("[작업 변경]");
+    int no = Prompt.inputInt("번호? ");
+
+    Task task = findByNo(no);
+
+
+    if (task == null) {
+      System.out.println("해당 번호의 작업이 없습니다.");
+      return;
+    }
+
+    String content = Prompt.inputString(String.format("내용(%s)? ", task.content));
+    Date deadline = Prompt.inputDate(String.format("마감일(%s)? ", task.deadline));
+
+    String stateLabel = getStatusLabel(no);
+
+    System.out.printf("상태(%s)?\n", stateLabel);
+    System.out.println("0: 신규");
+    System.out.println("1: 진행중");
+    System.out.println("2: 완료");
+    int status = Prompt.inputInt("> ");
+
+    String owner = null;
+    while (true) {
+      owner = Prompt.inputString(String.format(
+          "담당자(%s)?(취소: 빈 문자열) ", task.owner));
+      if (memberHandler.exist(owner)) {
+        break;
+      } else if (owner.length() == 0) {
+        System.out.println("작업 변경을 취소합니다.");
+        return; // 메서드 실행을 즉시 종료!
+      }
+      System.out.println("등록된 회원이 아닙니다.");
+    }
+
+    String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
+    if (input.equalsIgnoreCase("n") || input.length() == 0) {
+      System.out.println("작업 변경을 취소하였습니다.");
+      return;
+    }
+
+    task.content = content;
+    task.deadline = deadline;
+    task.status = status;
+    task.owner = owner;
+
+    System.out.println("작업를 변경하였습니다.");
+  }
+
+  public void delete() {
+    System.out.println("[작업 삭제]");
+    int no = Prompt.inputInt("번호? ");
+
+    int index = indexOf(no);
+
+    
+
+    if (index == -1) {
+      System.out.println("해당 번호의 작업이 없습니다.");
+      return;
+    }
+
+    String input = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
+    if (input.equalsIgnoreCase("n") || input.length() == 0) {
+      System.out.println("작업 삭제를 취소하였습니다.");
+      return;
+    }
+
+    for (int i = index + 1; i < this.size; i++) {
+      this.tasks[i - 1] = this.tasks[i];
+    }
+    this.tasks[--this.size] = null;
+
+    System.out.println("작업를 삭제하였습니다.");
+  }
+  
+  private Task findByNo(int no) {
+    for (int i = 0; i < this.size; i++) {
+      if (this.tasks[i].no == no) {
+        return this.tasks[i];
+        
+      }
+    }
+    return null;
+  }
+  
+  private int indexOf(int no) {
+
+    for (int i = 0; i < this.size; i++) {
+      if (this.tasks[i].no == no) {
+        return  i;
+        
+      }
+    }
+    return -1;
+  }
+  
+  private String getStatusLabel(int status) {
+    switch (status) {
+      case 1:
+        return "진행중";
+       
+      case 2:
+        return  "완료";
+      
+      default:
+        return "신규";
+    }
+  }
+  
+  
+  private String promptOwner(MemberHandler memberHandler, String ownerName) {
+   
+    while (true) {
+      String label = String.format("만든이%s(취소:빈 문자열)",
+          ownerName != null ? "(" + ownerName + ")" : "");
+      String owner = Prompt.inputString(label);
+      if (memberHandler.exist(owner)) {
+        return owner;
+        
+      } else if (owner.length() == 0) {
+        System.out.println("작업 등록 을 취소합니다.");
+        return null; // 메서드 실행을 즉시 종료!
+      }
+      System.out.println("등록된 회원이 아닙니다.");
+    }
+  }
+  
+  private String promptMembers(MemberHandler memberHandler , String members) {
+    String newMembers = "";
+    while (true) {
+      String member = Prompt.inputString(String.format(
+          "팀원?(완료: 빈 문자열) ",
+          members != null ? "(" + members + ")" : ""));
+      if (memberHandler.exist(member)) {
+        if (members.length() > 0) {
+          members += ",";
+        }
+        members += member;
+        continue;
+      } else if (member.length() == 0) {
+        break;
+      } 
+      System.out.println("등록된 회원이 아닙니다.");
+    }
+    return newMembers;
+  }
+} 
+  
