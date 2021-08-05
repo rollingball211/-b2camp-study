bb40d71e1dd34503f3e2471e559e449c6ebb6bc0
rollingball211 <jingoo211@gmail.com>
2021-08-05 14:53:13 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/handler/BoardList2.java
+++ study-project/app/src/main/java/com/eomcs/pms/handler/BoardList2.java
@@ -3,49 +3,89 @@ package com.eomcs.pms.handler;
 import com.eomcs.pms.domain.Board;
 
 public class BoardList2 {
-  static final int MAX_LENGTH = 5;
-  Board[] boards = new Board[MAX_LENGTH];
+  
+  static class Node {
+    Board board;
+    Node next;
+
+    public Node(Board board) {
+      this.board = board;
+    }
+  }
+  
+  Node head;
+  Node tail;
   int size = 0;
 
 public void add(Board board) {  
-  if (size == boards.length) {
-    Board [] arr = new Board[boards.length + (boards.length / 2)];
-    for (int i = 0; i < size; i++) {
-      arr[i] = boards[i];
-    }
-    boards = arr; //board 확장
+  Node node = new Node(board);
+
+  if (head == null) {
+    tail = head = node;
+  } else {
+    // 기존에 tail이 가리키는 마지막 노드의 next 변수에 새 노드 주소를 저장한다.
+    tail.next = node;
+
+    // 새로 만든 노드를 마지막 노드로 설정한다. 
+    tail = node;
   }
-this.boards[this.size++] = board;
+
+  size++;
 }
 
 public Board[] toArray() {
-  Board [] arr = new Board[this.size]; // 배열에 저장된 값을 담을 정도의 크기를 가진 새 배열을 만듬.
-  for (int i = 0; i<this.size ; i++) { // 배열에 저장된 값을 새 배열에 복사함.
-    arr[i] = boards[i];
+  //배열에 저장된 값을 담을 정도의 크기를 가진 새 배열을 만듬.
+  Board [] arr = new Board[this.size];
+  Node node = head;
+
+  int i = 0;
+  while (node != null){
+    arr[i++] = node.board;
+    node = node.next;
   }
-  return arr; //새 배열 리턴.
+  return arr;
 }
 
 public Board findByNo(int no) {
- for (int i = 0; i< this.size; i++) {
-   if (boards[i].no == no) {
-     return boards[i];
-   }
- }
+ 
+  Node node = head;
+  while (node != null) {
+    if (node.board.no == no) {
+      return node.board;
+    }
+    node = node.next;
+  }
   return null;
 }
 
 public Boolean remove(Board board) {
-  int index = indexOf(board);
-  if( index == -1) {
-    return false;
+  Node node = head;
+  Node prev = null;
+
+  while (node != null) {
+    if (node.board == board) { //노드에 들어있는 객체와 같다면
+      if (node == head) { //삭제할 노드가 첫번째 노드라면.
+        head = node.next;//head가 두번째 노드를 가르키게 한다.
+      } else {
+        prev.next = node.next; // 이전 노드를 다음 노드와 연결한다.
+      }
+
+      node.next = null; // 삭제할 노드가 더이상 다음 노드를 카리키지 않게 함.
+
+      if (node == tail) { // 삭제할 현재 노드가 마지막 노드라면
+        tail = prev; // 이전 노드를 마지막 노드로 설정한다.
+      }
+      size--;
+     return true;
+    }
+
+    // 현재 노드가 아니라면,
+    prev = node; // 현재 노드의 주소를 prev 변수에 저장하고,
+    node = node.next; // node 변수에는 다음 노드의 주소를 저장한다.
   }
+
   
-  for (int i = index + 1; i < this.size; i++) {
-    this.boards[i - 1] = this.boards[i];
-  }
-  this.boards[--this.size] = null;
-  return true;
+  return false;
 }
 
 
