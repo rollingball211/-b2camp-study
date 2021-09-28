e35ce871592e5387a403d9d3bb8f125dfdd6fb35
rollingball211 <jingoo211@gmail.com>
2021-09-28 11:16:39 +0900

    .

---
+++ study-project/app-client/README.md
@@ -0,0 +1,119 @@
+# 19-d. 데이터 관리 서버 만들기 : 프로토콜 정의 및 적용
+
+
+이번 훈련에서는,
+- **네트워크 API** 를 이용하여 데스크톱 애플리케이션을 클라이언트/서버 구조로 변경한다.
+
+데스크톱(desktop) 애플리케이션은,
+- 다른 애플리케이션과 연동하지 않고 단독적으로 실행한다.
+- 보통 PC나 노트북에 설치해서 사용한다.
+- 예) MS-Word, Adobe Photoshop, 메모장 등
+
+클라이언트(Client)/서버(Server) 애플리케이션은,
+- 줄여서 C/S 애플리케이션이라 부른다.
+- 클라이언트는 서버에게 서비스나 자원을 요청하는 일을 한다.
+- 서버는 클라이언트에게 자원이나 서비스를 제공하는 일을 한다.
+
+
+## 훈련 목표
+- 통신 프로토콜을 이해한다.
+- `extract method` 리팩토링 기법을 연습한다.
+
+## 훈련 내용
+- 응답 프로토콜을 변경하고 그에 맞게 구현한다.
+- 응답을 수신하는 코드를 별도의 메서드로 분리한다.
+
+### 요청 프로토콜
+
+```
+요청 데이터 규칙: 
+명령(UTF-8 문자열) CRLF
+JSON 데이터(UTF-8 문자열) CRLF  <== 명령어에 따라 선택 사항
+
+예) 게시글 목록 요청
+/board/list CRLF
+
+예) 게시글 상세 요청
+/board/detail CRLF
+{"no": 1} CRLF
+
+예) 게시글 등록 요청
+/board/add CRLF
+{
+    "title": "제목", 
+    "content": "내용", 
+    "writer": {
+        "no": 1, "name": "홍길동"
+    }
+} CRLF
+
+예) 게시글 변경 요청
+board/update CRLF
+{
+    "no": 1
+    "title": "제목", 
+    "content": "내용"
+} CRLF
+
+예) 게시글 삭제 요청
+/board/delete CRLF
+{"no": 1} CRLF
+```
+
+### 응답 프로토콜
+
+```
+응답 데이터 규칙: 
+처리상태(success | fail) CRLF
+JSON 데이터(UTF-8 문자열) CRLF  <== 처리 결과에 따라 선택 사항
+
+
+예) /board/list 요청에 대한 응답
+success CRLF
+게시글 목록에 대한 JSON 데이터 CRLF
+
+
+예) board/detail 요청에 대한 응답
+success CRLF
+{
+    "no": 1
+    "title": "제목", 
+    "content": "내용", 
+    "writer": {
+        "no": 1, "name": "홍길동"
+    },
+    "registeredDate": "2021-01-01",
+    "viewCount": 11,
+    "like": 5
+} CRLF
+
+예) board/add 요청에 대한 응답
+success CRLF
+
+예) board/update 요청에 대한 응답
+success CRLF
+
+예) board/delete 요청에 대한 응답
+success CRLF
+```
+
+## 실습
+
+### 1단계 - 프로토콜에 맞춰 게시글 데이터의 저장을 요청한다.
+
+- com.eomcs.pms.domain.Member 클래스 가져오기
+- com.eomcs.pms.domain.Board 클래스 가져오기
+- `com.eomcs.pms.ClientApp` 변경
+    - `addBoard()` 메서드 추가 
+        - 프로토콜에 맞춰 Board 객체를 JSON 데이터로 바꿔 서버에 보내기
+        - 서버의 응답 결과를 출력하기
+
+### 2단계 - 프로토콜에 맞춰 게시글 데이터의 조회를 요청한다.
+
+- `com.eomcs.pms.ClientApp` 변경
+    - `detailBoard()` 메서드 추가
+        - 프로토콜에 맞춰 Board 객체를 요구하기
+        - 서버에서 보내온 JSON 데이터로 Board 객체로 바꿔 출력하기
+
+## 실습 결과
+- src/main/java/com/eomcs/pms/ClientApp.java 변경
---
+++ study-project/app-client/src/main/java/com/eomcs/pms/ClientApp.java
@@ -0,0 +1,101 @@
+package com.eomcs.pms;
+
+import java.sql.Date;
+import java.util.HashMap;
+import com.eomcs.pms.domain.Board;
+import com.eomcs.pms.domain.Member;
+import com.eomcs.request.RequestAgent;
+import com.eomcs.util.Prompt;
+
+public class ClientApp {
+
+  static RequestAgent requestAgent;
+
+  public static void main(String[] args) throws Exception {
+
+
+    System.out.println("[PMS 클라이언트]");
+
+
+    requestAgent = new RequestAgent("127.0.0.1", 8888);
+
+
+    while (true) {
+      String input = Prompt.inputString("명령> ");
+
+      if (input.equals("/board/add")) {
+        addBoard();
+      } else if (input.equals("/board/detail")) {
+        detailBoard();
+      } else {
+        requestAgent.request(input, null);
+
+        if (requestAgent.getStatus().equals("success")) {
+          String result = requestAgent.getObject(String.class);
+          System.out.println(">>> " + result);
+
+
+        } else {
+          System.out.println("명령 요청 실패!");
+        }
+      }
+
+      if (input.equalsIgnoreCase("quit")) {
+        break;
+      }
+    }
+
+    requestAgent.close();
+
+    Prompt.close();
+  }
+
+  private static void addBoard() throws Exception {
+    Board board = new Board();
+    board.setNo(1);
+    board.setTitle("제목1");
+    board.setContent("내용1");
+    board.setRegisteredDate(Date.valueOf("2021-1-1"));
+
+    Member m = new Member();
+    m.setNo(100);
+    m.setName("aaa");
+    m.setEmail("aaa@test.com");
+
+    board.setWriter(m);
+    //서버에 요청
+    requestAgent.request("/board/add", board);
+
+    //서버가 보낸 결과 확인
+    if (requestAgent.getStatus().equals("success")) {
+      System.out.println("게시글 저장 성공!");
+    } else {
+      System.out.println("게시글 저장 실패!");
+    }
+
+
+  }
+
+  private static void detailBoard() throws Exception {
+
+    HashMap<String,Object> map = new HashMap<>();
+    map.put("no", 100);
+
+
+    requestAgent.request("/board/detail", map);
+
+
+    //서버가 보낸 결과 확인
+    if (requestAgent.getStatus().equals("success")) {
+      Board board = requestAgent.getObject(Board.class);
+      System.out.println(board);
+
+    } else {
+      System.out.println("게시글 조회 실패!");
+    }
+
+
+
+  }
+
+}
