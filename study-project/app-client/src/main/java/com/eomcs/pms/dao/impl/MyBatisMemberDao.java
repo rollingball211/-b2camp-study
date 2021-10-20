7fcd64875799fc6397b5145b9a2084f0c62c5df3
rollingball211 <jingoo211@gmail.com>
2021-10-20 16:55:28 +0900

    rewind

---
+++ study-project/app-client/src/main/java/com/eomcs/pms/dao/impl/MyBatisMemberDao.java
@@ -0,0 +1,77 @@
+package com.eomcs.pms.dao.impl;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import com.eomcs.pms.dao.MemberDao;
+import com.eomcs.pms.domain.Member;
+
+// 역할
+// - 회원 데이터를 DBMS 서버를 통해 관리한다.
+//
+public class MyBatisMemberDao implements MemberDao {
+
+  Connection con;
+  SqlSession sqlSession;
+
+  public MyBatisMemberDao(SqlSession sqlSession) {
+    this.sqlSession = sqlSession;
+  }
+
+  @Override
+  public void insert(Member member) throws Exception {
+    sqlSession.insert("MemberMapper.insert");
+    sqlSession.commit();
+  }
+
+  @Override
+  public List<Member> findAll() throws Exception {
+    return sqlSession.selectList("MemberMapper.findAll");
+
+
+  }
+
+
+  @Override
+  public Member findByNo(int no) throws Exception {
+    return sqlSession.selectOne("MemberMapper.findByNo");
+  }
+
+  @Override
+  public Member findByName(String name) throws Exception {
+
+    List<Member> list = sqlSession.selectList("MemberMapper.findByName",name);
+    if (list.size() > 0) {
+      return list.get(0);
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  public Member findByEmailAndPassword(String email, String password) throws Exception {
+    HashMap<String,Object> params = new HashMap<>();
+    params.put("email", email);
+    params.put("password", password);
+
+    return sqlSession.selectOne("MemberMapper.findByEmailAndPassword",params);
+  }
+
+
+  @Override
+  public void update(Member member) throws Exception {
+
+    sqlSession.update("MemberMapper.update",member);
+    sqlSession.commit();
+  }
+
+
+  @Override
+  public void delete(int no) throws Exception {
+    sqlSession.update("MemberMapper.delete",no);
+    sqlSession.commit();
+  }
+}
+
+
